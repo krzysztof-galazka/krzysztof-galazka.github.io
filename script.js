ff5782383fa1c1d0fee0b7cbc98f9336095bd6d3
krzysztof-galazka <kgalazka@vp.pl>
2020-08-11 20:28:49 +0200

    second change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'http://secure-everglades-29173/v1/task/';
+  const trelloApiRoot = 'http://secure-everglades-29173/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
